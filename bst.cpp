048349a19400c3d2169b10cb2e93e3f67a1adbef
yusukemh <yusukemh@hawaii.edu>
2021-11-08 19:15:20 -1000

    before operation

--- bst.cpp
+++ bst.cpp
@@ -36,6 +36,49 @@ class BST {
             }
         }
 
+        Node* searchHelper(Node* curr, int key) {
+            while (curr && curr->key != key) {
+                if (key < curr->key) {
+                    curr = curr->left;
+                } else {
+                    curr = curr->right;
+                }
+            }
+            return curr;
+        }
+
+        bool isEmpty() {
+            if(!root) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        Node* minHelper() {
+            /*
+            Returns the pointer to the node with the smallest key
+            */
+            Node* curr = this->root;
+            while(curr->left) {
+                curr = curr->left;
+            }
+            return curr;
+        }
+
+        Node* maxHelper() {
+            /*
+            Returns the pointer to the node with the maximum key
+            */
+            Node* curr = this->root;
+            while(curr->right) {
+                curr = curr->right;
+            }
+            return curr;
+        }
+
+        
+
     public:
         void insert(int key) {
             Node* node = new Node;
@@ -84,6 +127,34 @@ class BST {
             printf("\n");
         }
 
+        void search(int key) {
+            /*
+            Returns the pointer to the node whose key mathes the query.
+            */
+            Node* ret = searchHelper(this->root, key);
+            if (!ret) {
+                printf("The key does not exist in the tree.\n");
+            } else {
+                printf("The key exists in the tree.\n");
+            }
+        }
+
+        void min() {
+            if(isEmpty()) {
+                printf("This tree contains no keys.\n");
+            } else {
+                printf("Tree.min = %i\n", minHelper()->key);
+            }
+        }
+
+        void max() {
+            if(isEmpty()) {
+                printf("This tree contains no keys.\n");
+            } else {
+                printf("Tree.max = %i\n", maxHelper()->key);
+            }
+        }
+
 };
 
 int main(int argc, char* argv[]){
@@ -92,9 +163,14 @@ int main(int argc, char* argv[]){
     tree.insert(2);
     tree.insert(3);
     tree.insert(4);
+    tree.insert(10);
+    tree.insert(1);
     tree.inorder();
     tree.postorder();
     tree.preorder();
+    tree.search(10);
+    tree.min();
+    tree.max();
 }
 
 
